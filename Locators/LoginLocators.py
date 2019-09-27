0d6e3089667d0387d2e2c3d1ca3c5179c45f8ef7
Uriel Coria <u.coria@outlook.com>
2019-09-27 15:52:40 -0500

    cleaned up unused files

--- Locators/LoginLocators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginLocators(object):
-    """A class for main page locators. All main page locators should come here"""
-    USERNAME = (By.NAME, 'USERNAME_LOCATOR')
-    PASSWORD = (By.NAME, 'PASSWORD_LOCATOR')
-    LOGIN_BUTTON = (By.CLASS_NAME, 'LOCATOR_LOGIN_BUTTON')
